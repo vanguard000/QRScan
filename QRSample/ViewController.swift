fd58812faf0d0c44b69942c6ca674be4e117a577
MacOS <macos@macoss-mac-2.local>
2016-07-10 21:49:06 +0200

    Initial Commit

---
+++ QRSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRSample
+//
+//  Created by MacOS on 7/10/16.
+//  Copyright © 2016 MacOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
